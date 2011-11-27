b8dc16e629b952ef6e4a872aacb5dbde7651d848
James Mead <james@floehopper.org>
2011-11-27 21:39:26 +0000

    Make vim the default editor for everything.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,5 +2,17 @@ RBENVDIR="$HOME/.rbenv"
 PATH="$HOME/bin:$RBENVDIR/bin:$PATH"
 export PATH
 
+EDITOR="vim"
+export EDITOR
+
+GEM_EDITOR=$EDITOR
+export GEM_EDITOR
+
+BUNDLER_EDITOR=$EDITOR
+export BUNDLER_EDITOR
+
+GIT_EDITOR=$EDITOR
+export GIT_EDITOR
+
 # Boot rbenv if it exists
 if [ -s ~/.rbenv ] ; then eval "$(rbenv init -)" ; fi
