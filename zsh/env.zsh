babcace991a948c293a2df85cf56430ed7c090b0
James Mead <james@floehopper.org>
2011-12-13 13:15:45 +0000

    Use quieter cucumber format by default.
    So I can see the wood from the trees.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -14,5 +14,8 @@ export BUNDLER_EDITOR
 GIT_EDITOR=$EDITOR
 export GIT_EDITOR
 
+CUCUMBER_FORMAT=progress
+export CUCUMBER_FORMAT
+
 # Boot rbenv if it exists
 if [ -s ~/.rbenv ] ; then eval "$(rbenv init -)" ; fi
