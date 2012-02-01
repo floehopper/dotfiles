6f90b4bd554bb67eb6d4d610a56a5e99f2a6a043
James Mead <james@floehopper.org>
2012-02-01 14:35:43 +0000

    Added Ruby/Rails performance tweaks.
    As per https://gist.github.com/1688857

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -22,5 +22,13 @@ export LESS
 CUCUMBER_FORMAT=progress
 export CUCUMBER_FORMAT
 
+# Ruby/Rails performance tweaks as per https://gist.github.com/1688857
+# Supposedly better Rails performance at the cost of increased memory use
+export RUBY_HEAP_MIN_SLOTS=1000000
+export RUBY_HEAP_SLOTS_INCREMENT=1000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+export RUBY_GC_MALLOC_LIMIT=1000000000
+export RUBY_HEAP_FREE_MIN=500000
+
 # Boot rbenv if it exists
 if [ -s ~/.rbenv ] ; then eval "$(rbenv init -)" ; fi
