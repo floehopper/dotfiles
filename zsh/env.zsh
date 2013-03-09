8225f8a05443250fd79c6801d11646457ef3adaa
James Mead <james@floehopper.org>
2013-03-09 11:46:26 +0000

    Various changes to zsh configuration.
    * Vmail
    * Tmuxinator
    * Ruby GC settings

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -3,20 +3,23 @@ export GEM_EDITOR=$EDITOR
 export BUNDLER_EDITOR=$EDITOR
 export GIT_EDITOR=$EDITOR
 
+export VMAIL_BROWSER='elinks'
+export VMAIL_HTML_PART_READER='elinks -dump'
+
 # Prevent less from escaping xterm color control
 # characters that appear in Rails log file
 export LESS="-R"
 
 export CUCUMBER_FORMAT=progress
 
-# Ruby/Rails performance tweaks as per https://gist.github.com/1688857
+# Ruby/Rails performance tweaks as per https://gist.github.com/4524945
 # Supposedly better Rails performance at the cost of increased memory use
-export RUBY_HEAP_MIN_SLOTS=1000000
-export RUBY_HEAP_SLOTS_INCREMENT=1000000
-export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=1000000000
-export RUBY_HEAP_FREE_MIN=500000
+export RUBY_FREE_MIN=500000
+export RUBY_HEAP_MIN_SLOTS=40000
 
 eval "$(rbenv init -)"
 
-export PATH=".bundle/bin:$HOME/bin:$PATH"
+export PATH=".bundle/bin:$HOME/bin:/usr/local/heroku/bin:$PATH"
+
+[[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
