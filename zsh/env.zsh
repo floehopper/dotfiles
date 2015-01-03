94705440284ca11129f4dec280e3328538d9c1f9
James Mead <james@floehopper.org>
2015-01-03 18:32:03 +0000

    Remove Mono/FSharp-related config.
    This may have been needed when I was experimenting with the RTLSDR, but
    I don't have any of that installed on my new Yosemite install.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -7,10 +7,6 @@ export GIT_EDITOR=$EDITOR
 # characters that appear in Rails log file
 export LESS="-R"
 
-# For Mono/FSharp
-export MONO_GAC_PREFIX="/usr/local"
-
-
 eval "$(rbenv init -)"
 
 export PATH=".bundle/bin:~/node_modules/.bin/:$HOME/bin:/usr/local/heroku/bin:$PATH"
