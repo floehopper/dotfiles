affc4d668ebbf971fd20b76489829bdf31a91d8d
James Mead <james@floehopper.org>
2015-01-03 18:27:19 +0000

    Remove Ruby performance tweaks.
    It feels as if these may be out-of-date for recent versions of Ruby and in any
    case it seems wrong to use the same settings for all versions of Ruby given
    that I'm using rbenv to run many different versions of Ruby.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -9,20 +9,6 @@ export LESS="-R"
 
 #export CUCUMBER_FORMAT="progress"
 
-# Ruby performance tweaks from @zetter
-
-
-export RUBY_HEAP_SLOTS_INCREMENT=1000000
-export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-export RUBY_GC_MALLOC_LIMIT=1000000000
-export RUBY_HEAP_FREE_MIN=500000
-
-# Changes needed for Ruby 2.1
-# export RUBY_FREE_MIN=$RUBY_HEAP_FREE_MIN
-export RUBY_GC_HEAP_FREE_SLOTS=$RUBY_HEAP_FREE_MIN
-# export RUBY_HEAP_MIN_SLOTS=1000000
-export RUBY_GC_HEAP_INIT_SLOTS=1000000
-
 export IPLAYER_OUTDIR="Downloads"
 
 # For Mono/FSharp
