ff512aacb3baf8a4f73aa4031975fa72657393eb
James Mead <james@floehopper.org>
2015-01-03 19:36:17 +0000

    Fix zsh config.
    There are no longer any zsh functions and so these lines were causing an error.

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -1,13 +1,4 @@
-# add my functions to the front of the function load path
-# so my functions take precedence over any others
-fpath=(~/.zsh/functions $fpath)
-
 source ~/.zsh/prompt.zsh
 source ~/.zsh/history.zsh
 source ~/.zsh/aliases.zsh
 source ~/.zsh/completion.zsh
-
-# the -U turns off the use of any aliases defined when the function is actually loaded
-# this is so that the function doesn't have to worry about whether a command it wants
-# to use has been aliased to something else
-autoload -U ~/.zsh/functions/*(:t)
