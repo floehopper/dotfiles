deb574fbe21abd075f40e21632d4ca09ad076a11
James Mead <james@floehopper.org>
2015-01-03 19:13:12 +0000

    Remove bundler-related aliases and function.
    I _think_ the bin stubs functionality is now provided by the global
    bundler config, but I can always add stuff back if I need it.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,22 +1,10 @@
 alias e="mate ."
 alias ls="ls -l"
 
-# depends on the bundler gem
-# bundle exec is provided as a *function* `be`
-alias bi="bundle install --path .bundle/gems --binstubs .bundle/bin"
-alias bo="bundle open"
-alias bc="bundle console"
-alias bu="bundle update"
-
 # depends on git
 alias s="git st"
 alias c="git ci"
 alias a="git add"
 alias d="git diff"
-
-# the following relies on the `be` *function*
-alias ruby='be ruby'
-alias irb='be irb'
-
 
 alias vlc='/Applications/VLC.app/Contents/MacOS/VLC -I rc'
