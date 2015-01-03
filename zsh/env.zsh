0f6ec940a2a3aef56542feaeb935d15f9a151192
James Mead <james@floehopper.org>
2015-01-03 18:31:13 +0000

    Remove iPlayer-related config.
    I don't have any iPlayer software installed in my new Yosemite install.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -7,8 +7,6 @@ export GIT_EDITOR=$EDITOR
 # characters that appear in Rails log file
 export LESS="-R"
 
-export IPLAYER_OUTDIR="Downloads"
-
 # For Mono/FSharp
 export MONO_GAC_PREFIX="/usr/local"
 
