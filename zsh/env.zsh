7f7b28da10f1992a13e87bccc3dc0893e186e550
James Mead <james@floehopper.org>
2017-08-23 09:52:25 +0100

    Remove PATH additions
    I don't need any of these at the moment.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -5,10 +5,3 @@ export EDITOR="atom --foreground"
 # Prevent less from escaping xterm color control
 # characters that appear in Rails log file
 export LESS="-R"
-
-export PATH="/usr/local/sbin:$PATH"
-export PATH="/System/Library/CoreServices:$PATH"
-export PATH="/Users/jamesmead/bin:$PATH"
-export PATH="/usr/local/heroku/bin:$PATH"
-export PATH="./node_modules/.bin:$PATH"
-export PATH="./.bundle/bin:$PATH"
