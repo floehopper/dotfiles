dca66af29cd6c0f81b76708dd68e322da82e98a7
James Mead <james@floehopper.org>
2025-02-01 12:00:17 +0000

    Add basic .railsrc file
    This includes the default options that will be passed to `rails new`.

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -53,6 +53,10 @@ in
     # See https://developer.1password.com/docs/ssh/agent
     home.file."${userHome}/.1password/agent.sock".source = config.lib.file.mkOutOfStoreSymlink "${userHome}/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
 
+    home.file."${userHome}/.railsrc".text = ''
+      --minimal
+    '';
+
     programs.zsh = {
       enable = true;
       oh-my-zsh = {
